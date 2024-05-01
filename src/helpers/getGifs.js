82a91b2b47f503c3d88a9dcdbecb1edc62275b4e
Andres Quintero <andres_f.quintero_s@uao.edu.co>
2024-04-30 19:50:13 -0500

    variables de entorno

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=R30qqAaUzZ009KSEexuj3tC5ITIN8UfP&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_api_key}=${category}&limit=20`;
     const resp = await fetch(url);
     const {data = []} = await resp.json();
     const gifs = data.map( img => ({
